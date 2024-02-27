7da91b790b18382c6135972e9463862b0c372425
GamerFox2000 <98109784+GamerFox2000@users.noreply.github.com>
2024-02-27 16:14:25 +0100

    Update 1.1.py

--- 1.1.py
+++ 1.1.py
@@ -73,7 +73,7 @@ elif elegir == 2:
         elif intento > numero_secreto:
             print("Demasiado alto. Intenta de nuevo.")
         else:
-            print(f"¡Felicidades! ¡Adivinaste el número en {intentos} intentos!")
+            print(f"¡Felicidades ",nombre "! ¡Adivinaste el número en {intentos} intentos!")
             break
       volver_a_jugar = input("¿Quieres jugar de nuevo? (s/n): ")
       if volver_a_jugar.lower() == "s":
